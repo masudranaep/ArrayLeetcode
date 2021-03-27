84585821306a79fe1d93877d67a37e043638a111
Md Masud Rana <77980851+masudranaep@users.noreply.github.com>
2021-03-27 09:39:34 +0600

    Teemo Allocking

---
+++ 495.Teemo Allocking.java
@@ -0,0 +1,13 @@
+Input:[1,4],2 /[1,2]
+
+output:4      /3
+
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        if(timeSeries.length == 0) return 0;
+        int totalTime = 0;
+        for(int i = 0; i < timeSeries.length -1; i++)
+            totalTime += Math.min(timeSeries[i+1] - timeSeries[i], duration);
+        return totalTime + duration;
+    }
+}
