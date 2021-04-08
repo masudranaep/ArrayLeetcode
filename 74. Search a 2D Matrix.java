de781886102a540fa23397a4c56bab240a9f13e2
Md Masud Rana <77980851+masudranaep@users.noreply.github.com>
2021-04-08 08:38:24 +0600

    Search a 2D Matrix

---
+++ 74. Search a 2D Matrix.java
@@ -0,0 +1,24 @@
+   public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length == 0) return false;
+        
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int low = 0;
+        int hig = (n * m) - 1;
+        
+        while(low <= hig){
+            int mid = (low + ( hig - low) / 2);
+                if(matrix[mid / m][mid % m] == target){
+                return true;
+ }
+            if(matrix[mid/m][mid % m] < target){
+                low = mid  + 1;
+            }else{
+                hig = mid - 1;
+            }
+          
+            }
+return false;
+        
+    }
+}
