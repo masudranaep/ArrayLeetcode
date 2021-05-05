af58b89d58f2963871585fa29a06c50358a3afd5
Md Masud Rana <77980851+masudranaep@users.noreply.github.com>
2021-05-05 08:14:23 +0600

    Shortest Unsorted Continuous Subarray

---
+++ 581. Shortest Unsorted Continuous Subarray.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+      int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length -1; i++){
+            if(nums[i] > nums[i+1]){
+                min = Math.min(min, nums[i+1]);
+            }
+        }
+        for(int i = nums.length-1; i > 0; i--){
+            if(nums[i] < nums[i -1]){
+                max = Math.max(max, nums[i-1]);
+            }
+        }
+        int l = 0, r = 0;
+        for(int i = 0; i < nums.length -1; i++){
+            if(nums[i] > min){
+                l =i;
+                break;
+            }
+        }
+        for(int i = nums.length -1; i >= 0; i--){
+            if(nums[i] < max){
+                r =i;
+                break;
+            }
+        }
+        return r - l > 0 ? r -l+1:0;
+    }
+}
