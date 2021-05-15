d8af643367f99c39df2d0272bfa228b43b6ccaf3
Md Masud Rana <77980851+masudranaep@users.noreply.github.com>
2021-05-15 09:00:27 +0600

    Single Number

---
+++ Single Number.java
@@ -0,0 +1,15 @@
+    public int singleNumber(int[] nums) {
+      HashSet<Integer> set = new HashSet<Integer>();
+        for(int i : nums){
+            if(set.contains(i)){
+                set.remove(i);
+            }else{
+                set.add(i);
+            }
+        }
+        for(int i : set){
+            return i;
+        }
+        return -1;
+    }
+}
