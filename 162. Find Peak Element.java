3b384e82be356d488c8d4e325ce4c87b56918aae
Md Masud Rana <77980851+masudranaep@users.noreply.github.com>
2021-05-24 07:56:09 +0600

    Find Peak Element

---
+++ 162. Find Peak Element.java
@@ -0,0 +1,12 @@
+    public int findPeakElement(int[] nums) {
+    int L = 0, R = nums.length-1;
+        while(L < R){
+            int mid = L + (R -L)/2;
+            if(nums[mid] > nums[mid +1])
+                R = mid;
+            else
+                L = mid +1;
+}
+        return L;
+        
+    }
